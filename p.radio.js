d6df3beaa6be711993cbddea8dabed44f2b541be
Lesego864 <tshipulesego@gmail.com>
2021-08-13 02:28:38 +0200

    started the pizza cart changes

---
+++ p.radio.js
@@ -0,0 +1,10 @@
+const sAddbuttonElement = document.querySelector(".add1button");
+const sMinbuttonElement = document.querySelector(".min1button");
+const mAddbuttonElement = document.querySelector(".Add2button");
+const mMddbuttonElement = document.querySelector(".Min2button");
+const lAddbuttonElement = document.querySelector(".ADD3button");
+const lMddbuttonElement = document.querySelector(".MIN3button");
+
+sAddbuttonElement.addEventListener("click", function() {
+    alert(value)
+})

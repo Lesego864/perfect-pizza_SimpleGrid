3feb358b85c8f7bd679d86dc8216dabb059f001c
Lesego864 <tshipulesego@gmail.com>
2021-08-18 01:17:32 +0200

    fixed the checkout button

--- p.radio.js
+++ p.radio.js
@@ -13,12 +13,14 @@ const q2Element = document.querySelector(".Q2");
 const q3Element = document.querySelector(".Q3");
 const b1Element = document.querySelector(".buy_one")
 const checkElement = document.querySelector(".checkbtn");
+const amountElement = document.querySelector(".the_amount_paid");
 const paymentElement = document.querySelector(".payment");
 const messageElement = document.querySelector(".message");
 const menuElement = document.querySelector(".menu");
 const buy1Element = document.querySelector(".buy_one");
 const buy2Element = document.querySelector(".buy_two");
 const buy3Element = document.querySelector(".buy_three");
+const cashElement = document.querySelector(".cash")
 
 
 
@@ -107,11 +109,14 @@ function largePizzaSubCost() {
 };
 checkElement.addEventListener('click', checkOutButton);
 function checkOutButton() {
-
+    amountElement.classList.remove("hide");
+};
+cashElement.addEventListener('click', payButton);
+function payButton() {
     if (totalCost > paymentElement.value) {
-        messageElement.innerHTML = "Insufficient Funds!";
+        messageElement.innerHTML = "Sorry - That was not enough money!";
     } else if (totalCost < paymentElement.value) {
-        messageElement.innerHTML = "Thank You For Your Support!";
+        messageElement.innerHTML = "Enjoy your pizzas!";
     }
 };
 buy1Element.addEventListener('click', lekkerbutton1);
